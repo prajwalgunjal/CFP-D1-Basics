9d2589e0bd1da80c5c723cc1b2081c3e48bacf96
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-07 22:34:18 +0530

    lagrgest number added

---
+++ FindLargest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode
+{
+    public class FindLargest
+    {
+        public void FindLargestNumber() {
+            Console.WriteLine("Enter first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int b = int.Parse(Console.ReadLine());  
+            Console.WriteLine("Enter third number");
+            int c = int.Parse(Console.ReadLine());
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a + "is a biggest number ");
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine(b + " is a biggest number");
+            }
+            else
+            {
+                Console.WriteLine(c + "is a biggest number");
+            }
+        }
+    }
+}
