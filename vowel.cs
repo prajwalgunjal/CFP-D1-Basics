0826c853980d35b04d1fcbddecd00dc0f3cb6416
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-07 22:32:53 +0530

    vowel program added

---
+++ vowel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode
+{
+    public class vowel
+    {
+        public void Checkvowel() {
+            Console.WriteLine("Enter a character to check");
+            string a = Console.ReadLine();
+            if (a == "a" || a == "e" || a == "i" ||a == "o" || a=="u") {
+                Console.WriteLine("its a vowel");
+            }
+            else
+            {
+                Console.WriteLine("not vowel");
+            }
+        }
+    }
+}
