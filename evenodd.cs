60f25f895958e1c193948e295cbde1f39835636d
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-07 22:30:46 +0530

    evenodd program added

---
+++ evenodd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode
+{
+    public class evenodd
+    {
+        public void oodeven()
+        {
+            Console.WriteLine("Enter a number: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0) Console.WriteLine("Even");
+            else Console.WriteLine("odd");
+        }
+    }
+}
