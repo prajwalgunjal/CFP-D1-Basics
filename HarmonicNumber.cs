c2d809cffdde4c26b2b7b5faf5b7218a445c7fbc
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-07 22:24:19 +0530

    harmonicnumber program added

---
+++ HarmonicNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode
+{
+    public class HarmonicNumber
+    {
+        public void HarmonicNumberr() {
+            Console.WriteLine("Enter the value of n to find harmonic number ");
+            int harmonicValue = int.Parse(Console.ReadLine());
+            double harmonicNumber = 0.0;
+            if (harmonicValue != 0)
+            {
+                for (int i = 1; i <= harmonicValue; i++) {
+                    harmonicNumber = harmonicNumber+(double)1 / i;
+                    Console.WriteLine(harmonicNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("please enter values greater than 0 ");
+            }
+        }
+    }
+}
