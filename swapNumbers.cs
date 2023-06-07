ae4b2229a0eae2b5652e0bdc21c1fb9d6e73589b
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-07 22:29:30 +0530

    swapping of two number added

---
+++ swapNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode
+{
+    public class swapNumbers
+    {
+        public void swap()
+        {
+            Console.WriteLine("Enter first number:-");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:- ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("before swapping :- ");
+            Console.WriteLine("a= " + a + "b = " +b);
+
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After swapping :- ");
+            Console.WriteLine("a= " + a + "b = " + b);
+
+
+        }
+    }
+}
