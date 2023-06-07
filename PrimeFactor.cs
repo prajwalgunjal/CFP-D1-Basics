95e7b24ed98ec38b33e4438807f3990e98740746
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-07 22:25:50 +0530

    prime factors program added

---
+++ PrimeFactor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode
+{
+    internal class PrimeFactor
+    {
+        public void prime() {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i < num; i++)
+            {
+                while (num % i == 0)
+                {
+                    num = num / i;
+                    Console.WriteLine(i);
+                }
+            }
+            if (num != 1)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
