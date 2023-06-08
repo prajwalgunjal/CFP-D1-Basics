bfa973f628d3d57f378411050c6058e106f06a1e
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-08 15:01:18 +0530

    fibonacci series program added

---
+++ FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacciseries() {
+            Console.WriteLine("Enter a number till that number series will print");
+            int num = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            int sum;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 2; i <= num; i++) {
+                sum = a + b;
+                Console.WriteLine(sum+" ");
+                a = b;
+                b = sum;
+            }
+        }
+    }
+}
