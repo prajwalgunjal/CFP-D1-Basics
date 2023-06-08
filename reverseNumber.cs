abf0f556f2fcfd3ec437376bea9fed06a5317c72
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-08 15:11:24 +0530

    reverse added

---
+++ reverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCode
+{
+    public class reverseNumber
+    {
+        public void revserse()
+        {
+            Console.WriteLine("Enter a number: - ");
+            int a = int.Parse(Console.ReadLine());
+            int rev = 0;
+            while (a != 0)
+            {
+                int last = a % 10;
+                rev = rev * 10 + last;
+                a = a / 10;
+            }
+            Console.WriteLine(rev);
+        }
+    }
+}
